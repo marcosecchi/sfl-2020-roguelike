b695e76948d80bab6e34d851edc0a3c520d12978
Marco Secchi <loziosecchi@gmail.com>
2020-12-11 20:55:21 +0100

    Add comments.

--- Assets/Scripts/KnightController.cs
+++ Assets/Scripts/KnightController.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class KnightController : MonoBehaviour
 {
+	// Velocità di movimento
 	public float speed = 3;
+	
+	// Velocità di rotazione
 	public float rotationSpeed = 3;
 	
+	// Riferimento interno al CharacterController
 	private CharacterController _controller;
 	
-    // Start is called before the first frame update
     void Start()
-    {
+	{
+		// Cerca il CharacterController e lo assegna alla variabile corrispondente
 	    _controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
-    {
+	{
+		// Ruota il personaggio a seconda del movimento orizzontale del thumbstick
 	    Vector3 rot = Vector3.up * rotationSpeed * Input.GetAxis("Horizontal");
 	    transform.Rotate(rot);
 	    
+		// Muove il personaggio utilizzando il CharacterController
 	    Vector3 mov = transform.forward * speed * Input.GetAxis("Vertical") * Time.deltaTime;
 	    _controller.SimpleMove(mov);
     }
