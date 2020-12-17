a889680401930cbfa4deabb7fc075a0b6d87aae7
Marco Secchi <loziosecchi@gmail.com>
2020-12-17 12:53:30 +0100

    Add Weapon system.

---
+++ Assets/Scripts/MouseRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotator : MonoBehaviour
+{
+
+	public float rotationSpeed = 1.5f;
+	
+    void Update()
+    {
+	    Vector3 rot = Vector3.up * rotationSpeed * Input.GetAxis("Mouse X");
+	    transform.Rotate(rot);
+    }
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+	public Transform slot1;
+	public Transform slot2;
+	
+	public GameObject startingWeapon1Prefab;
+	public GameObject startingWeapon2Prefab;
+	
+    void Start()
+    {
+	    EquipWeapon1(startingWeapon1Prefab);
+	    EquipWeapon2(startingWeapon2Prefab);
+    }
+
+	public void EquipWeapon1(GameObject prefab)
+	{
+		foreach(Transform child in slot1)
+		{
+			Destroy(child.gameObject);
+		}
+		if(prefab != null)
+		{
+			Instantiate(prefab, slot1.position, slot1.rotation, slot1);	
+		}
+	}
+
+	public void EquipWeapon2(GameObject prefab)
+	{
+		foreach(Transform child in slot2)
+		{
+			Destroy(child.gameObject);
+		}
+		if(prefab != null)
+		{
+			Instantiate(prefab, slot2.position, slot2.rotation, slot2);
+		}
+	}
+	
+	
+	
+	
+}
