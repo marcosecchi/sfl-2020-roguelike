be96d6bf6835b2e6b55bfa8a933b69eecb8a5560
Marco Secchi <loziosecchi@gmail.com>
2020-12-18 10:33:13 +0100

    Add Inventory System.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+	
+	private List<GameObject> equipmentList;
+	
+    void Start()
+    {
+	    equipmentList = new List<GameObject>();
+    }
+    
+	public void AddEquipment(GameObject equipment)
+	{
+		equipmentList.Add(equipment);
+	}
+}
---
+++ Assets/Scripts/InventoryPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryPickup : MonoBehaviour
+{
+	public GameObject equipmentPrefab;
+	
+	protected void OnTriggerEnter(Collider other)
+	{
+		if(!other.CompareTag("Player")) return;
+		Inventory inv = other.GetComponent<Inventory>();
+		if(inv == null) return;
+		//		inv.AddEquipment(equipmentPrefab);
+		
+		Destroy(gameObject);
+	}
+}
